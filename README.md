b50adfefd5227229c8f0747c19fefcf5a6241b72
Aum Pauskar <aumpauskar111@gmail.com>
2023-08-05 22:42:52 +0530

    Able to add books in SQL

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 - Python and pip
 	1. flask `pip install flask`
 	2. jinja2 `pip install Jinja2`
+	3. mysql connector `pip install mysql-connector-python`
 - MySQL (tested with v 8.0.33)
 
+## Setting up
+
+- Environment variables
+To set up this application you need to set up the environment variable in the windows machine. To do this search environment variable in the search menu and add a new key in it. The key must be named **sql_key**. Note that you might want to restart you comptuer after setting up the environment variable key.
 ## Notes
-The software is created within a python virtual environment.
+The software is created within a python virtual environment and using a Windows NT kernel (Win 11).
--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, redirect
+# importing libraries
+from flask import Flask, render_template, redirect, request
+import mysql.connector
+import os
+sql_passwd = os.environ.get("sql_key")
+
+
 
 app = Flask(__name__)
 
+# ------------------------------
+# rendering applications
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -17,6 +25,27 @@ def contact():
 @app.route('/student')
 def student():
 	return render_template('student.html')
+# ------------------------------
+
+# populating database
+# lms database -> books table
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+	print("Function running")
+	# print(request.form['book-name'])
+	book_name = request.form['book-name']
+	book_author = request.form['book-author']
+	stock = request.form['stock']
+
+	connection = mysql.connector.connect(host='localhost', user='root', password=sql_passwd, database='lms')
+	cursor = connection.cursor()
+
+	cursor.execute('INSERT INTO books (NAME, AUTHOR, STOCK) VALUES (%s, %s, %s)', (book_name, book_author, stock))
+	connection.commit()
+	return f'''Book added successfully
+	Book name: {book_name}
+	Book author: {book_author}
+	Stock: {stock}'''
 
 if __name__ == '__main__':
 	app.run(debug=True)
