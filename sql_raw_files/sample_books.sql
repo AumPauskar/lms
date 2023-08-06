79cf7f89b0d6a1012695257edc6ab8182f11cf74
Aum Pauskar <aumpauskar111@gmail.com>
2023-08-06 18:32:02 +0530

    Renamed sql raw to initial_setup+ created sample_books

---
+++ sql_raw_files/sample_books.sql
@@ -0,0 +1,25 @@
+-- SAMPLE BOOK INFO
+-- THIS VALUE IS A SAMPLE DATASET TO TEST THE PROGRAM
+
+USE lms;
+INSERT INTO books (NAME, AUTHOR, STOCK) VALUES
+("The Night Circus", "Erin Morgenstern", 12),
+("The Power of One", "Bryce Courtenay", 7),
+("Neverwhere", "Neil Gaiman", 15),
+("The Goldfinch", "Donna Tartt", 9),
+("The Book Thief", "Markus Zusak", 11),
+("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 18),
+("Dune", "Frank Herbert", 5),
+("The Handmaid's Tale", "Margaret Atwood", 14),
+("The Alchemist", "Paulo Coelho", 20),
+("1984", "George Orwell", 8),
+("The Picture of Dorian Gray", "Oscar Wilde", 6),
+("The Hobbit", "J.R.R. Tolkien", 17),
+("To Kill a Mockingbird", "Harper Lee", 10),
+("Brave New World", "Aldous Huxley", 4),
+("Pride and Prejudice", "Jane Austen", 13),
+("The Catcher in the Rye", "J.D. Salinger", 3),
+("The Great Gatsby", "F. Scott Fitzgerald", 16),
+("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 19),
+("The Lord of the Rings: The Fellowship of the Ring", "J.R.R. Tolkien", 2),
+("The Da Vinci Code", "Dan Brown", 6);
